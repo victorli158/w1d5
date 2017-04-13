70478f5cf2c685f5ade25a1d7e8638be8fe4a9d9
Victor Li <victor.li158@gmail.com>
2017-04-13 09:36:00 -0700

    First commit

--- skeleton/lib/knight.rb
+++ skeleton/lib/knight.rb
@@ -1,5 +1,5 @@
 require_relative '00_tree_node'
-
+require 'byebug'
 
 class KnightPathFinder
 
@@ -14,14 +14,14 @@ class KnightPathFinder
     [2, 1]
   ]
 
-  attr_reader :start
+  attr_reader :start, :tree
   attr_accessor :visited_positions
 
   def initialize(start_square = [0,0])
     @start = start_square
     @board = self.class.create_board
-    @tree = self.build_move_tree
     @visited_positions = [@start]
+    @tree = self.build_move_tree
   end
 
   def self.create_board
@@ -29,22 +29,25 @@ class KnightPathFinder
   end
 
   def build_move_tree
-    seed = create_node(start)
-    queue = [seed]
-    until visited_positions.length == 64
-      seed.children.each do |child|
-        queue << create_node(child)
-
-
-
-
+    root = create_node(start)
+    queue = [root]
+    until queue.empty?
+      first = queue.shift
+      first.children.each do |child|
+        new_node = create_node(child.value)
+        queue.push(new_node)
+        new_node.parent = first
+        debugger
+      end
+    end
+    root
   end
 
   def create_node(pos)
-    seed = PolyTreeNode.new
-    new_move_positions(start).each do |move|
+    seed = PolyTreeNode.new(pos)
+    new_move_positions(pos).each do |move|
       visited_positions << move #is this right?
-      seed.children.add_child(move)
+      seed.add_child(PolyTreeNode.new(move))
     end
     seed
   end
@@ -54,14 +57,17 @@ class KnightPathFinder
 
   def self.valid_moves(pos)
     pos_moves = MOVERS.map { |move| [pos[0] + move[0], pos[1] + move[1]] }
-    pos_moves.reject! { |move| move[0] > 7 || move[0] < 0 || move[1] > 7 || move[1] < 0 }
-    pos_moves
+    pos_moves.reject { |move| move[0] > 7 || move[0] < 0 || move[1] > 7 || move[1] < 0 }
   end
 
   def new_move_positions(pos)
     moves = self.class.valid_moves(pos)
-    moves.reject! { |move| visited_positions.include?(move)}
-    moves
+    moves.reject { |move| visited_positions.include?(move)}
   end
 
+end
+
+if __FILE__ == $PROGRAM_NAME
+  k = KnightPathFinder.new([0,0])
+  puts k.tree
 end
